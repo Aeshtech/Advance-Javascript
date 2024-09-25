a310460ef737790783be29272f35c8703c9a70ca
Ashish Sharma <dev.aeshtech@gmail.com>
2024-09-25 22:46:30 +0530

    Created curry function to make other function into curried version

---
+++ curry.js
@@ -0,0 +1,21 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function (...nextArgs) {
+        return curried.apply(this, args.concat(nextArgs));
+      };
+    }
+  };
+}
+
+function sum(a, b, c) {
+  return a + b + c;
+}
+
+const curriedSum = curry(sum);
+
+console.log(curriedSum(1)(2)(3)); // Output: 6
+console.log(curriedSum(1, 2)(3)); // Output: 6
+console.log(curriedSum(1, 2, 3)); // Output: 6
