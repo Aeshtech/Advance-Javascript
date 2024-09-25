da75e01d2172c45a04ef82c834bd7cdf3c3651a4
Ashish Sharma <dev.aeshtech@gmail.com>
2024-09-25 11:45:41 +0530

    Created map-polyfill.js

---
+++ map-polyfill.js
@@ -0,0 +1,9 @@
+// Array.map((num, i, arr) => { })
+
+Array.prototype.myMap = function (cb) {
+let newArr= [] ;
+for (let i = 0; i < this.length; i++) {
+newArr.push(cb(this[i], i, this));
+}
+return newArr;
+};
