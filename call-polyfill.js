9f70f93cb5301ae7e52823df18c9525db24f49d4
Ashish Sharma <dev.aeshtech@gmail.com>
2024-09-25 12:14:16 +0530

    Created call-polyfill.js

---
+++ call-polyfill.js
@@ -0,0 +1,18 @@
+const car1 = {
+color: 'Silver',
+name: 'Tata Curvv'
+}
+
+function purchaseCar(currency, price){
+console.log(`I have purchased ${this.color} - ${this.name} car for ${currency}${price}`);
+}
+
+Function.prototype.myCall = function (context = {}, ...args) {
+if (typeof this !== "function") {
+throw new Error(this + "It's not Callable");
+}
+context.fn = this;
+context.fn(...args);
+};
+
+purchaseCar.myCall(car1, "₹", '20,000,00');
