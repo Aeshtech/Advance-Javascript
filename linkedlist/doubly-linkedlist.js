12ee82f5cbed20673196d0efd1f57f7a64508561
ashish <dev.aeshtech@gmail.com>
2024-10-03 23:50:57 +0530

    created doubly linkedlist

--- linkedlist/doubly-linkedlist.js
+++ linkedlist/doubly-linkedlist.js
@@ -1,27 +1,28 @@
-/* Circular Linkedlist is identical to Singly Linkedlist for various operations but it also provide direct access to
-   head node from the last node. In Circular Linkedlist the next pointer of last node must point to the first node.
+/* Doubly linkedlist is a advance version of singly-linkedlist as it provide traversal to back&forth i.e in doubly linkedlist
+   we can traverse two way. In DLL except head & last node, each node contains prev and next node references in order to
+   provide two way traversal and due to two references on each node it needs more memory than singly-linkedlist.
 */
 
-import { Node } from "./node.js";
+import { DoublyNode } from "./node.js";
 
-class CircularLinkedList {
+class DoublyLinkedList {
   head = null;
 
   add(data) {
-    const newNode = new Node(data);
+    const newNode = new DoublyNode(data);
 
     if (this.head === null) {
       this.head = newNode;
-      newNode.next = this.head;
       return this.head;
     }
 
     let curr = this.head;
-    while (curr && curr.next !== this.head) {
+    while (curr.next !== null) {
       curr = curr.next;
     }
     curr.next = newNode;
-    newNode.next = this.head;
+    newNode.prev = curr;
+    return this.head;
   }
 
   addAtPos(data, index) {
@@ -29,53 +30,52 @@ class CircularLinkedList {
       throw new Error("Invalid Index: Index cannot be negative");
     }
 
-    const newNode = new Node(data);
+    const newNode = new DoublyNode(data);
 
-    //if inserting at head (index = 0)
     if (index === 0) {
+      // Special case: Inserting at the head
       newNode.next = this.head;
-      // Traverse to the last node to update its next pointer to the new head
-      let curr = this.head;
-      while (curr.next !== this.head) {
-        curr = curr.next;
+      if (this.head) {
+        this.head.prev = newNode;
       }
-      curr.next = newNode; // Last node should point to the new head
       this.head = newNode;
       return this.head;
     }
 
     let curr = this.head;
     let count = index - 1;
-
-    while (count > 0 && curr.next !== this.head) {
+    while (count > 0 && curr.next !== null) {
       curr = curr.next;
       count--;
     }
 
     if (count === 0) {
       newNode.next = curr.next;
-      // curr.next is the node at which we have to add the newNode
+      if (curr.next) {
+        curr.next.prev = newNode;
+      }
+      newNode.prev = curr;
       curr.next = newNode;
     } else {
       throw new Error("Index out of bounds");
     }
+    return this.head;
   }
 
   print() {
     if (this.head === null) return console.log("Empty List");
     let curr = this.head;
     const arr = [];
-    do {
-      arr.push({ data: curr.data, next: curr.next });
+    while (curr !== null) {
+      arr.push({ data: curr.data, next: curr.next, prev: curr.prev });
       curr = curr.next;
-    } while (curr !== this.head);
-
+    }
     console.log(arr);
   }
 }
 
 try {
-  const cll = new CircularLinkedList();
+  const cll = new DoublyLinkedList();
   cll.add("Hello");
   cll.add("World");
   cll.add(10);
@@ -84,7 +84,7 @@ try {
   console.log("Inserting elements at pos 0, 2 and at end of the list:");
   cll.addAtPos("NewNode1", 0);
   cll.addAtPos("NewNode2", 2);
-  cll.addAtPos("NewNode3", 6);
+  cll.addAtPos("NewNode3", 4);
   cll.print();
 } catch (error) {
   console.log({ error });
