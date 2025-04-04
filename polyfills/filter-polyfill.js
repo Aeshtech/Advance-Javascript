d47947dcb4eecf877a10e0147a5a167a0fa22d79
Ashish Sharma <dev.aeshtech@gmail.com>
2025-04-04 14:51:26 +0530

    Updated filter-polyfill.js

--- polyfills/filter-polyfill.js
+++ polyfills/filter-polyfill.js
@@ -2,7 +2,7 @@
 Array.prototype.myFilter = function (cb) {
 let newArr = [] ;
 for (let i = 0; i < this.length; i++) {
-if(cb(this[i])){
+if(cb(this[i], i, this)){
 newArr.push(this[i]);
 }}
 return newArr;
